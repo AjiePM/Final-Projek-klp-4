b1ad295bc65ba2a29804ede238296bd016cbacdd
Ajie Prana Mesa <109120170+AjiePM@users.noreply.github.com>
2022-09-21 16:08:44 +0800

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store'
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+// import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
 ReactDOM.render(
   <Router>
